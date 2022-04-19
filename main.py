f30bc9c535d779f38375ffd6b77d2b805508b8f9
Wahbi-K <wahbi.k.elbouri@gmail.com>
2022-04-19 12:22:06 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 19 12:15:57 2022
+
+@author: Wahbi K. El-Bouri
+"""
+
+# Code for Infarct Core Segmentation Using Vitrea - Kausik Chaterjee Collaboration
+
