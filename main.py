2b52b948853e3e926ef2d16211ade927a066a825
Wahbi-K <wahbi.k.elbouri@gmail.com>
2022-04-19 12:53:20 +0100

    Load in dicom image and plot
    Loading in image and plotting a simple colourmap

--- main.py
+++ main.py
@@ -7,3 +7,20 @@ Created on Tue Apr 19 12:15:57 2022
 
 # Code for Infarct Core Segmentation Using Vitrea - Kausik Chaterjee Collaboration
 
+import numpy as np
+import matplotlib.pyplot as plt
+import pydicom
+from pydicom.data import get_testdata_file
+import sys
+import cv2
+
+if __name__=='__main__':
+
+    path = r'C:\Users\wahbi\OneDrive - The University of Liverpool\Infarct Core Segmentation Project\Vitrea\Test1\DIACOM\WSDTI1R4\SC3I4ZVV'
+    
+    sys.path.append(path)
+    
+    # Try for 1 image to start with - can loop this later or do it in parallel
+    ds = pydicom.dcmread(path+ "\I1090000")
+    
+    plt.imshow(ds.pixel_array, cmap=plt.cm.bone) 
