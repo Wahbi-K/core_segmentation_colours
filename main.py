535003d33c6a651e85540d99d051344b9c19ec82
Wahbi-K <wahbi.k.elbouri@gmail.com>
2022-06-03 10:20:45 +0100

    Change to local files

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ Created on Tue Apr 19 12:15:57 2022
 
 # Code for Infarct Core Segmentation Using Vitrea - Kausik Chaterjee Collaboration
 import argparse
+import time
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -17,14 +18,14 @@ from model import Clusterer
 import constants
 from inference import Inferer
 
-TRAIN_DIR = r"C:/Users/rashe/Downloads/segmentation_test_data"
-TEST_DIR = r"C:/Users/rashe/Downloads/segmentation_test_data"
+TRAIN_DIR = r"C:\Users\wahbi\OneDrive - The University of Liverpool\Infarct Core Segmentation Project\Vitrea\Test1\DIACOM\WSDTI1R4\SC3I4ZVV"
+TEST_DIR = r"C:\Users\wahbi\OneDrive - The University of Liverpool\Infarct Core Segmentation Project\Vitrea\Test1\DIACOM\WSDTI1R4\SC3I4ZVV"
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--num_train_images', default=10, type=int)
-    parser.add_argument('--num_test_images', default=10, type=int)
-    parser.add_argument('--num_clusters', default=10, type=int)
+    parser.add_argument('--num_train_images', default=50, type=int)
+    parser.add_argument('--num_test_images', default=50, type=int)
+    parser.add_argument('--num_clusters', default=8, type=int)
     parser.add_argument('--clustering_method', default='KNN', type=str)
     args = parser.parse_args()
     return args
@@ -46,6 +47,7 @@ def main(args):
     inferer.save_inferred_plots()
 
 if __name__=='__main__':
-
+    t = time.time()
     sys.path.append(TRAIN_DIR)
     main(parse_args())
+    print('Elapsed time: ' + str(time.time()-t) +' s')
